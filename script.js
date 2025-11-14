2a241bb1ba0012ff27d1fa16b37072f737c547fe
Sang <Sangg2109@gmail.com>
2025-11-15 02:20:15 +0700

    sua loi

--- script.js
+++ script.js
@@ -15,3 +15,44 @@ window.addEventListener("DOMContentLoaded", () => {
     loadHTML("header", "header.html");
     loadHTML("footer", "footer.html");
 });
+
+//scroll top button
+const btn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 200) {
+        btn.style.display = "flex";
+    } else {
+        btn.style.display = "none";
+    }
+};
+
+btn.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
+//scroll left right
+document.querySelectorAll(".products").forEach(section => {
+    const list = section.querySelector(".product-list");
+    const items = list.querySelectorAll(".product-item");
+    const leftBtn = section.querySelector(".scroll-left");
+    const rightBtn = section.querySelector(".scroll-right");
+
+    // Ẩn sản phẩm từ sản phẩm thứ 5
+    items.forEach((item, index) => {
+        if (index >= 4) item.style.display = "none";
+    });
+
+    // Cuộn theo từng item
+    const itemWidth = items[0].offsetWidth + 20; // 20 là gap
+
+    rightBtn.onclick = () => {
+        list.scrollLeft += itemWidth;
+    };
+
+    leftBtn.onclick = () => {
+        list.scrollLeft -= itemWidth;
+    };
+});
