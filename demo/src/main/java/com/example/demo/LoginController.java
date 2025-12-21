dbc45a39762566c27ff80efdc0af6b3aefaa246b
Sang <Sangg2109@gmail.com>
2025-12-21 23:45:26 +0700

    thêm các class về Product

---
+++ demo/src/main/java/com/example/demo/LoginController.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "LoginController", value = "/login")
+public class LoginController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        //AuthService as = new AuthService();
+        if ("abc".equals(username) && "123".equals(password)) {
+            resp.sendRedirect("index.jsp");
+        } else {
+            req.setAttribute("error", "Sai username or password");
+            req.getRequestDispatcher("login.jsp").forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("login.jsp").forward(req,resp);
+
+
+        }
+    }
