363d2e6bae7c2845763df3aca6aac502a4202727
Sang <Sangg2109@gmail.com>
2026-01-10 21:48:46 +0700

    thêm thành công dữ liệu lên web

--- demo/src/main/java/services/ProductService.java
+++ demo/src/main/java/services/ProductService.java
@@ -1,4 +1,5 @@
 package services;
+import dao.ProductDao;
 import model.Product;
 import java.util.List;
 import java.util.ArrayList;
@@ -6,8 +7,9 @@ import java.util.ArrayList;
 public class ProductService {
 
 
+    ProductDao dao = new ProductDao();
     public List<Product> getListProduct() {
-        return new ArrayList<>();
+        return dao.getListProduct();
     }
 
     public Product getProduct(int id) {
